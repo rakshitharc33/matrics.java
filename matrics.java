bb63e7c3886a8e78f82727dc694144e83e10878c
rakshitharc33 <97296466+rakshitharc33@users.noreply.github.com>
2022-01-18 20:21:02 +0530

    matrix

---
+++ matrics.java
@@ -0,0 +1,16 @@
+class Program
+{
+  public static void main(String[] args)
+  {
+    int a[][]={{2,6,8},{4,8,6},{4,6,8}};
+    for(i=0;i<3;i++)
+    {
+      for(j=0;j<3;j++)
+      {
+        System.out.print(arr[i][j]+" ");
+      }
+      System.out.println();
+    }
+  }
+}
+  
